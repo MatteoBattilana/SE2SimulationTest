ca536a9a7136e57548628d930c44c0994dc068dd
Matteo <matbat96@gmail.com>
2017-12-06 15:06:28 +0100

    Fixed typo error

--- router.js
+++ router.js
@@ -1,48 +1,52 @@
-var express    = require('express');        // call express
+var express = require('express'); // call express
 var DB = {};
-var port = 8080;        // set our port
+var port = 8080; // set our port
 
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
-router.post('/assignement', function(req, res,next) {
-    var assignement_type = req.body.assignement_type;
-    var test_id = req.body.test_id;
-    var student_id = req.body.student_id;
-    var assignement_content = req.body.assignement_content;
-    //Save into fake db
-    if(assignement_type && test_id && student_id && assignement_content){
-    	if(!DB[test_id])
-    		DB[test_id] = {};
-	DB[test_id][student_id]=req.body;
-    	res.status(200)
-    	   .json({ message: 'Stored in our DB' });
-    }
-    else{
-    	var err = new Error('Missing parameters!');
-    	err.status = 403;
-    	next(err);
-    }
+router.post('/assignment', function(req, res, next) {
+  var assignment_type = req.body.assignment_type;
+  var test_id = req.body.test_id;
+  var student_id = req.body.student_id;
+  var assignment_content = req.body.assignment_content;
+  //Save into fake db
+  if (assignment_type && test_id && student_id && assignment_content) {
+    if (!DB[test_id])
+      DB[test_id] = {};
+    DB[test_id][student_id] = req.body;
+    res.status(200)
+      .json({
+        message: 'Stored in our DB'
+      });
+  } else {
+    var err = new Error('Missing parameters!');
+    err.status = 403;
+    next(err);
+  }
 });
 
-router.get('/assignement/:test_id', function(req, res,next) {
-    var test_id = req.params.test_id;
-    if(test_id){
-    	res.status(200)
-    	   .json(DB[test_id]);
-    }
-    else{
-    	var err = new Error('Missing parameters!');
-    	err.status = 403;
-    	next(err);
-    }
+router.get('/assignment/:test_id', function(req, res, next) {
+  var test_id = req.params.test_id;
+  if (test_id) {
+    res.status(200)
+      .json(DB[test_id]);
+  } else {
+    var err = new Error('Missing parameters!');
+    err.status = 403;
+    next(err);
+  }
 });
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+  res.json({
+    message: 'hooray! welcome to our api!'
+  });
 });
 
 router.post('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+  res.json({
+    message: 'hooray! welcome to our api!'
+  });
 });
 
 
